a0349b5391af1824425abcf50c7572b62af9a66a
Kirill Klenov <horneds@gmail.com>
2021-06-07 16:41:42 +0300

    Bump version: 3.1.2 → 3.2.0

--- muffin_apiclient/__init__.py
+++ muffin_apiclient/__init__.py
@@ -8,7 +8,7 @@ from muffin.plugins import BasePlugin
 from apiclient import APIClient
 
 
-__version__ = "3.1.2"
+__version__ = "3.2.0"
 __project__ = "muffin-apiclient"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
